fcea9a72f54b06d7716c64a4e9ba77f75fbf6452
Ruoyi <lohruoyi0502@gmail.com>
2026-03-17 22:19:02 +0800

    Added a reusable PageHeader.vue to main

---
+++ src/PageHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page-header">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageHeader",
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .page-header {
+        justify-content: left;
+    }
+
+    .page-header h1 {
+        margin: 0;
+        font-size: 40px;
+        color: #003D7C;
+    }
+
+</style>
+
