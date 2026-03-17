8042559640d8055ce5a8e738ca15ed4d4ef4a176
sherthemain <shermainenus@gmail.com>
2026-03-17 20:12:31 +0800

    create listing title, description

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <div id="app">
-    <TheNavbar />
-    <main class="main-content">
-      <router-view />
-    </main>
-    <TheFooter />
-  </div>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<script>
-import TheFooter from '@/components/TheFooter.vue'
-import TheNavbar from '@/components/TheNavbar.vue'
-
-export default {
-  name: 'App',
-  components: { TheFooter, TheNavbar },
-}
-</script>
-
-<style scoped>
-#app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-.main-content {
-  flex: 1;
-}
-</style>
+<style scoped></style>
