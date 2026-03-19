60ea03f8ec3d412c1b910b6c260b24503efb972a
sherthemain <shermainenus@gmail.com>
2026-03-20 00:16:24 +0800

    rebased wrongly, added back App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <TheNavbar />
+    <main class="main-content">
+      <router-view />
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import TheFooter from '@/components/TheFooter.vue'
+import TheNavbar from '@/components/TheNavbar.vue'
+
+export default {
+  name: 'App',
+  components: { TheFooter, TheNavbar },
+}
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main-content {
+  flex: 1;
+}
+</style>
