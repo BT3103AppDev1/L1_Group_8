d7caf177f94396694d62b3f28a636b14abd478c5
fl4shagent <ham23012006@gmail.com>
2026-03-28 21:24:12 +0800

    hard code for a mock user to wire create listing + my listing logic
    Using Firebase data

--- src/auth.js
+++ src/auth.js
@@ -1,13 +1,17 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase.js';
 
-let currentUser = auth.currentUser;
+// TODO: remove hardcode when auth is live
+let currentUser = { uid: 'zrxX7Bt3kZSaPYpyBuaokbJz47i1' };
 const listeners = [];
 
+/* actual logic
+let currentUser = auth.currentUser;
 onAuthStateChanged(auth, (user) => {
   currentUser = user;
   listeners.forEach(listener => listener(user));
 });
+*/
 
 function getCurrentUser() {
   return currentUser;
