627d2c79acff307c4dff49c7af9a2cc96d227d86
sherthemain <shermainenus@gmail.com>
2026-03-29 23:30:47 +0800

    edited the style on EditListing

--- src/views/EditListing.vue
+++ src/views/EditListing.vue
@@ -253,6 +253,31 @@ export default {
     align-items: center;
 
 }
+
+.photo {
+    width: 100%;
+    max-width: 500px;
+    margin: 0 auto 10px auto;
+    overflow: hidden;
+    border-radius: 10px;
+
+}
+
+.cropper-img {
+    width: 100%;
+    height: auto;
+    display: block;
+    object-fit: contain;
+}
+
+.cropper-actions {
+    margin: 10px 0;
+    display: flex;
+    gap: 10px;
+    justify-content: center;
+}
+
+
 .listing-card {
     width: 600px;
     border-radius: 10px;
