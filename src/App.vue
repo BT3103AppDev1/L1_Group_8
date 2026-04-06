ba93c0f80d9f2653337e0f00114efd4e33e078c7
XinYan <vlxy020304@gmail.com>
2026-04-06 20:44:01 +0800

    Fix header's profile picture not updating after change

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ export default {
         this._firestoreUnsubscribe = onSnapshot(userDocRef, (doc) => {
           if (doc.exists()) {
             const data = doc.data();
-            this.profilePicUrl = data.profilePicUrl || null;
+            this.profilePicUrl = data.profile_pic_url || null;
 
             const route = this.$route.path;
             if (!data.email_verified && route !== '/email-verification') {
