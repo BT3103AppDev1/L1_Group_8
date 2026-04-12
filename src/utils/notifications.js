c3073db235a33d3b953ef3ad3879cdd945fa2313
XinYan <vlxy020304@gmail.com>
2026-04-12 18:24:41 +0800

    Create a utility function to add a notification document

---
+++ src/utils/notifications.js
@@ -0,0 +1,26 @@
+import { db } from "@/firebase.js";
+import { collection, doc, Timestamp } from "firebase/firestore";
+
+export function addCreateNotifToBatch(batch, {
+    uid, 
+    type,
+    listing_id = null,
+    listing_title = null,
+    rating = null,
+    increase_in_points = null,
+    sgt_year_month = null,
+}) {
+    const notifRef = doc(collection(db, "notifications"));
+
+    batch.set(notifRef, {
+        uid,
+        type,
+        listing_id,
+        listing_title,
+        rating,
+        increase_in_points,
+        sgt_year_month,
+        is_sent: false,
+        created_at: Timestamp.now(),
+    })
+}
