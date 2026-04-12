d7f60309c7c40f38db74f89facf4a048b134b0d7
Ruoyi <lohruoyi0502@gmail.com>
2026-04-12 12:17:10 +0800

    Merge conflict

--- src/auth.js
+++ src/auth.js
@@ -1,25 +1,13 @@
 import { auth } from './firebase.js';
 import { onAuthStateChanged } from "firebase/auth";
 
-<<<<<<< HEAD
-// TODO: remove hardcode when auth is live
-//let currentUser = { uid: 'zrxX7Bt3kZSaPYpyBuaokbJz47i1' }; 
-const listeners = [];
-
-let currentUser = auth.currentUser;
-=======
 let currentUser = auth.currentUser;
 const listeners = [];
 
->>>>>>> edc99cad9c16ef02dfdf805bb5d30346722751d6
 onAuthStateChanged(auth, (user) => {
   currentUser = user;
   listeners.forEach(listener => listener(user));
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> edc99cad9c16ef02dfdf805bb5d30346722751d6
 
 async function getCurrentUser() {
   await auth.authStateReady();
