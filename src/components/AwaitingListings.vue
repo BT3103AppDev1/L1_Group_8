d27b5c11ee74256d44c069138f17bfe23040e77c
XinYan <vlxy020304@gmail.com>
2026-04-16 00:18:27 +0800

    Fixed AwaitingListings listedBy not rendering issue

--- src/components/AwaitingListings.vue
+++ src/components/AwaitingListings.vue
@@ -117,7 +117,7 @@ export default {
                         //Normalisee category to ensure it fit category button 
                         category: data.listing_category?.trim(),
                         //Make sure there is user table and then use the corresponding user_name
-                        listedBy: data.lister_name ?? "Unknown User",
+                        lister_name: data.lister_name ?? "Unknown User",
                         //Update date format into something more readable
                         postedOn: data.created_at?.toDate().toLocaleDateString("en-SG", {
                             year: "numeric",
